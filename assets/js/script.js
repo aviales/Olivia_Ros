07d79c595e15395a8e3553cf4698039cd94f08f3
Alonso Viales <avialesarrieta@gmail.com>
2021-04-15 17:59:26 -0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).redy(function(){
+  
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+      
+      var hash = this.hash;
+      
+      $("html, body").animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+  
+  $('[data-toggle="popover"]').popover();
+  
+});
